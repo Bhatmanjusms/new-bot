806f0803a1a6917ebc43da21d1d033479e9a66fa
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-26 22:59:57 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -360,13 +360,13 @@ def humanbytes(size):
 
 #link converter 
 
-shortz = shortzy.Shortzy(SHORTENER_API, "shareus.in")
+shortz = Shortzy(SHORTENER_API, "shareus.in")
 async def get_shortlink(link):
     if SHORTENER_API:
         if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
-            return await shortz.get_quick_link(link)
+            return await Shortzy.get_quick_link(link)
         else:
-            return await shortz.convert(link, silently_fail=False)
+            return await Shortzy.convert(link, silently_fail=False)
     return link
 
 
