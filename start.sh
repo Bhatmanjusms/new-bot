ffefca3a178b030878739cd3d64fffcc71287f02
Sahidmalik07 <108992799+Sahidmalik07@users.noreply.github.com>
2022-11-26 23:07:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sahidmalik07/verify2.git /verify2
+  git clone https://github.com/Akshay-Chand/new-bot.git /new-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /verify2
+  git clone $UPSTREAM_REPO /new-bot
 fi
-cd /verify2
+cd /new-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot....💥"
 python3 bot.py
